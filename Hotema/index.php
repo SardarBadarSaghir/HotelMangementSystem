06357271c17c5e73022db2702b939f6ffe11249a
SardarBadarSaghir <dataentrybadar@gmail.com>
2020-11-24 14:20:35 +0500

    hotema

--- Hotema/index.php
+++ Hotema/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-include "./componets/head.php";
-include "./signIn.php";
-include "./componets/footer.php";
 
-?>    
-
-
+</body>
 </html>
